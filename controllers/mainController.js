3de9bb672d32e0645dc18cdbf579a51201504faf
Jonathan <jonbezeau@gmail.com>
2017-07-24 11:33:31 -0700

    Finishing up work from a previous commit

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,9 +1,19 @@
 const mainPage = require("../views/mainPage");
 
+const courseModel = require('../models/courseModel.js');
+
 module.exports = function(req, res) {
 
     if(req.session.email){
-        res.send(mainPage());
+
+        courseModel.find({}).exec(function (error, response){
+            console.log(error);
+            console.log(response);
+
+            res.send(mainPage({courses: response}));
+            
+        });
+
         return;
     }
 
--- views/mainPage.js
+++ views/mainPage.js
@@ -9,25 +9,9 @@ module.exports = function(context) {
 
         <input type="button" value="Log out" onclick="location.href='/login/logout'">
 
-
-        <p>The following form can be used to create deliverables for courses that exist in the UVOSSO database:</p>
-        <form action="deliverable/createDeliverable" method="post">
-            <label for="name">Name: </label>
-                <input id="name" type="text" name="name" placeholder="Enter deliverable name here">
-            <label for "due_date">Due Date: </label>
-                <input id="due_date" type="Date" name="due_date" placeholder="Enter the due date here">
-            <label for="type">Type: <label>
-                <select name="type">
-                    <option value="assignment">Assignment</option>
-                    <option value="quiz">Quiz</option>
-                    <option value="midterm">Midterm</option>
-                    <option value-"final">Final</option>
-                    <option value="misc">Misc</option>
-            <label for="course">Course: </label>
-                <input id="course" type="text" name="course" placeholder="Enter the corresponding course name here">
-            <input type="submit" value="OK">
-        <form>
-
+        <pre>
+        ${context.courses}
+        </pre>
 
         `
     })
